d4df20fc1555d539feff27376b0b82e34ea45df7
araful39 <rajuslam39@gmail.com>
2025-05-13 17:22:38 +0600

    raju add

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat bot by Gemini',
+      title: 'Chat bot by Gemini make Raju',
       debugShowCheckedModeBanner: false,
       home: ChatScreen(),
     );
