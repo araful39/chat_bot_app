bb7cf50eeda1e08b641f284e6ce4cc55ec61b010
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2025-05-13 17:29:02 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chat bot by Gemini',
+      //raju
       debugShowCheckedModeBanner: false,
       home: ChatScreen(),
     );
